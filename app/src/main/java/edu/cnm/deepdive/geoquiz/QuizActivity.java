4d408ccc5a7b8a9c7690cbbcaf7c4d03bb585486
Todd A Nordquist <toddnordquist144@gmail.com>
2019-02-07 06:34:52 -0700

    Added acitivity log methods and device configuration corrections.

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -1,7 +1,9 @@
 package edu.cnm.deepdive.geoquiz;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
 
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "index";
   private Button mTrueButton;
   private Button mFalseButton;
   private Button mNextButton;
@@ -29,8 +33,12 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
 
+    if (savedInstanceState != null) {
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
     mTrueButton = findViewById(R.id.true_button);
@@ -57,6 +65,43 @@ public class QuizActivity extends AppCompatActivity {
       }
     });
     upDateQuestion();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
   }
 
   private void upDateQuestion() {
